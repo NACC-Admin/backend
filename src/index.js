860c67d3941e95897f7927ee20b40b33b00343c1
Mospheb <benjaminadeniyi@gmail.com>
2023-06-04 15:57:58 +0100

    remove ec2 conf files

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ var cors = require('cors')
 const app = express();
 app.use(bodyParser.json());
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 9090;
 
 //Middleware
 app.use(express.urlencoded({ extended: true}));
@@ -143,4 +143,4 @@ function authController (req, res) {
 }
 
 
-app.listen(port, () => console.log(`Listening on port 9090`+process.env.PORT || 8080));
+app.listen(port, () => console.log(`Listening on port 9090`+process.env.PORT || 9090));
